d2cdeabd3010ee50a0777cf7102b6ce18cf59496
Clanie1 <danybarociov@gmail.com>
2024-02-20 11:39:39 -0800

    init

---
+++ IOSapp/IOSapp/IOSappApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSappApp.swift
+//  IOSapp
+//
+//  Created by Daniel Barocio Valdez on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct IOSappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
